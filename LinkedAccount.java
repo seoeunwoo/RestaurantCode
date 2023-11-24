a96064c18aa56ceac90e4d73966172c064dbf10e
EunWoo <81130124+seoeunwoo@users.noreply.github.com>
2023-11-24 11:53:16 +0900

    Delete LinkedAccount.java

--- LinkedAccount.java
+++
@@ -1,7 +0,0 @@
-package hello.restaurant.restaurantconnection;
-
-public abstract class LinkedAccount {
-    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
-    public static final String USERNAME = "sa";
-    public static final String PASSWORD = "";
-}
