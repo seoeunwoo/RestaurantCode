b5520b70ba66444ad67d404f64205ceb12e095ce
EunWoo <81130124+seoeunwoo@users.noreply.github.com>
2023-11-24 11:59:25 +0900

    Add files via upload

---
+++ DB/LinkedAccount.java
@@ -0,0 +1,7 @@
+package hello.restaurant.restaurantconnection;
+
+public abstract class LinkedAccount {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
